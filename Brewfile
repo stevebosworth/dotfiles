64ba5f16c8acfdd5e60b74d31d964b4c16793321
stevebosworth <steve.bosworth@gmail.com>
2018-05-22 17:37:04 -0400

    Update brewfile and initial key repeat

--- Brewfile
+++ Brewfile
@@ -16,8 +16,6 @@ brew 'grc'
 brew 'imagemagick'
 brew 'jp2a'
 brew 'jq'
-brew 'libcaca', args: ['with-imlib2']
-brew 'libgit2'
 brew 'openssl'
 brew 'node'
 brew 'readline'
--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -18,7 +18,7 @@ chflags nohidden ~/Library
 
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write NSGlobalDomain InitialKeyRepeat -int 12 # normal minimum is 15 (225 ms)
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
