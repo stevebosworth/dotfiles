522fff07b73c81f629038447c20d346dac0893d6
stevebosworth <steve.bosworth@shopify.com>
2019-11-26 14:00:19 -0500

    Update brewfile and git config

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -40,8 +40,8 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 # Setup VS Code defaults
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
-$ defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
-$ defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
+# defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+# defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
 
 # Macvim defaults
 defaults write org.vim.MacVim ApplePressAndHoldEnabled -bool false
