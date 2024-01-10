663dab72d5dbba759c64c3a9bc75ddb459fec751
Steve Bosworth <stevebosworth@gmail.com>
2024-01-10 15:19:37 -0400

    Switch to separate neovim config from vim using lua

--- neovim/init.vim
+++
@@ -1,4 +0,0 @@
-" Use ~/.vimrc for neovim
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath=&runtimepath
-source ~/.vimrc
---
+++ neovim/lua/options.lua
@@ -0,0 +1,33 @@
+-- Hint: use `:h <option>` to figure out the meaning if needed
+vim.opt.clipboard = 'unnamedplus'   -- use system clipboard
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+vim.g.mapleader = ','             -- leader key
+vim.opt.selection = 'exclusive'     -- don't include the newline when yanking
+
+-- Folding
+vim.opt.foldmethod = 'indent'       -- fold based on indent level
+vim.opt.foldlevel = 20              -- open all folds by default
+
+-- Tab
+vim.opt.tabstop = 2                 -- number of visual spaces per TAB
+vim.opt.softtabstop = 2             -- number of spacesin tab when editing
+vim.opt.shiftwidth = 2              -- insert 2 spaces on a tab
+vim.opt.expandtab = true            -- tabs are spaces, mainly because of python
+vim.opt.autoindent = true           -- copy indent from current line when starting a new line
+vim.opt.smartindent = true          -- smarter indentation for C-like languages
+
+-- UI config
+vim.opt.number = true               -- show absolute number
+vim.opt.relativenumber = true       -- add numbers to each line on the left side
+vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true           -- open new vertical split bottom
+vim.opt.splitright = true           -- open new horizontal splits right
+vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
+
+-- Searching
+vim.opt.incsearch = true            -- search as characters are entered
+vim.opt.hlsearch = true             -- highlight matches
+vim.opt.ignorecase = true           -- ignore case in searches by default
+vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+
