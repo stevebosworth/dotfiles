7ca10fd78d681f4aa8b00c262ecf5208ccc48ba3
Steve Bosworth <steve.bosworth@shopify.com>
2024-12-05 00:01:49 -0500

    add avante with ollama configured as the provider

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -148,6 +148,44 @@ local plugins = {
       }
     end,
   },
+
+  -- Avante - AI Code Assistant
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- latest changes
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "hrsh7th/nvim-cmp",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
 
 require("lazy").setup(plugins)
